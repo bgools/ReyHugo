c1f3442d169dee297eff774fd38c94c4807c3e61
Bryan Goolsby <bgoolz@bgoolzpductions.home>
2020-08-23 01:56:13 -0400

    created components

--- ReyHugo2/src/app/app.module.ts
+++ ReyHugo2/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CaroselComponent } from './carosel/carosel.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BooksComponent } from './books/books.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CaroselComponent,
+    ProjectsComponent,
+    AboutComponent,
+    ContactComponent,
+    BooksComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
